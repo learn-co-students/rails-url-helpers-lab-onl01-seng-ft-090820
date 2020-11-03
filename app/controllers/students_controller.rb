d47bc5c2ec95ea842c05f9aa36cd50dd7b6aa1fa
David <dhutchis68516@gmail.com>
2020-11-03 13:34:44 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,21 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+      @active = "This student is currently active."
+      @inactive = "This student is currently inactive."
+  end
+
+  def activate
+    set_student
+    if @student.active == false
+      @student.active = true
+      @student.save 
+    else  
+      @student.active = false 
+      @student.save 
+    end 
+    redirect_to "/students/#{@student.id}"
   end
 
   private
